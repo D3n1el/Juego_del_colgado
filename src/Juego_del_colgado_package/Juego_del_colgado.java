b01492d9cc65bbbcd96b628961ba9c42ae2f4a1e
D3n1el <dani.alcaraz.p@gmail.com>
2024-11-18 19:58:04 +0100

    2- Rondas y palabra
    Se han eliminado algunas variables por ser innecesarias.
    El usuario ya puede introducir las rondas deseadas. También, ya puede introducir la palabra a adivinar

--- src/Juego_del_colgado_package/Juego_del_colgado.java
+++ src/Juego_del_colgado_package/Juego_del_colgado.java
@@ -12,7 +12,8 @@ public class Juego_del_colgado {
 		//Declaración de variables
 		int tj = 0; //Total de jugadores
 		int tr = 0; //Total de rondas
-		int ti = 0; //Total de intentos
+		String palabra = "";
+		String P = "";
 				
 		System.out.println("|---------------------------|");
 		System.out.println("|   El juego del ahorcado   |");
@@ -25,6 +26,21 @@ public class Juego_del_colgado {
 			if (tj >= 2) {break;} //Al poner un número válido, sale del bucle para seguir la instrucción después de dicho bucle.
 			System.out.println("¡NUMERO INVÁLIDO!");
 		}
+		
+		while(true) { //Bucle que impide la introducción de numeros inválidos
+			System.out.println("");
+			System.out.print("Indica el numero de rondas: ");
+				tr = s.nextInt();
+			if (tr >= 1) {break;} //Al poner un número válido, sale del bucle para seguir la instrucción después de dicho bucle.
+			System.out.println("¡NUMERO INVÁLIDO!");
+		}
+		
+		String x = s.nextLine(); //String que permite que en el siguiente se pueda realizar.
+		
+		System.out.println("Introduce la palabra a adivinar:");
+			palabra = s.nextLine();
+			palabra = palabra.toUpperCase();
+			palabra = palabra.replaceAll("[ABCDEFGHIJKLMNÑOPQRSTUVWXYZ]", "□");
 		
 	}
 

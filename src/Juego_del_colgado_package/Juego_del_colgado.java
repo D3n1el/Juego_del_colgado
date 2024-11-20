1585866da0f06f599b1e04e29385e6cb74353c3e
D3n1el <dani.alcaraz.p@gmail.com>
2024-11-20 19:21:42 +0100

    5- Verificacion de letra
    Funcionamiento de forma parcial de la sustitución del texto a adivinar excepto la palabra introducida por el usuario.

--- src/Juego_del_colgado_package/Juego_del_colgado.java
+++ src/Juego_del_colgado_package/Juego_del_colgado.java
@@ -33,13 +33,13 @@ public class Juego_del_colgado {
 		System.out.print("Introduce la palabra a adivinar: ");
 			palabra = s.nextLine(); //Guarda la palabra a adivinar
 			palabra = palabra.toUpperCase(); //Pasa todo el texto a mayúsculas
-			p = palabra.replaceAll("[ABCDEFGHIJKLMNÑOPQRSTUVWXYZ]", "_ "); //censura todas las letras a □
+			p = palabra.replaceAll("[ABCDEFGHIJKLMNÑOPQRSTUVWXYZ]", "_ "); //censura todas las letras por "_"
 		
 		for (int i = vidas.length - 1; i >= 0 ; i--) { //Contador de vidas
 			System.out.println("");
 			System.out.println(vidas[i]);
 			
-			for (int ii = 0 + 1; ii < tj; ii++) {
+			for (int ii = 1; ii <= tj; ii++) {
 				System.out.println("Turno para el jugador " + ii);
 				System.out.println(p);
 				
@@ -48,6 +48,19 @@ public class Juego_del_colgado {
 					String letra = s.nextLine();
 					letra = letra.toUpperCase();
 					
+					for (int iii = 0; iii < palabra.length(); iii++) {
+						char cpalabra = palabra.charAt(iii);
+						char cletra = letra.charAt(0);
+						
+						if (cletra == cpalabra) {
+							
+							String P = "(?i)(?![" + cletra + "])\\w";
+							p = palabra.replaceAll(P, " _");
+							System.out.println("¡Acertaste!");
+							System.out.println("");
+						}
+						
+					}
 				
 				
 			}

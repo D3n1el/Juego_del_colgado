6f2f518dc60bc11175b05e2a056395c04bbd3817
D3n1el <dani.alcaraz.p@gmail.com>
2024-11-21 01:09:45 +0100

    6- Sustitución por Array
    Se ha sustituido el String "sencillo" por uno que se basa en un Array de caracteres, el cual separa cada letra y las guarda en diferentes posiciones.

--- src/Juego_del_colgado_package/Juego_del_colgado.java
+++ src/Juego_del_colgado_package/Juego_del_colgado.java
@@ -12,7 +12,6 @@ public class Juego_del_colgado {
 		//Declaración de variables
 		int tj = 0; //Total de jugadores
 		String[] vidas = {"❤️", "❤️❤️", "❤️❤️❤️", "❤️❤️❤️❤️", "❤️❤️❤️❤️❤️", "❤️❤️❤️❤️❤️❤️"};
-		String palabra = "";
 		String p = "";
 				
 		System.out.println("|---------------------------|");
@@ -31,9 +30,18 @@ public class Juego_del_colgado {
 		
 		System.out.println("");
 		System.out.print("Introduce la palabra a adivinar: ");
-			palabra = s.nextLine(); //Guarda la palabra a adivinar
-			palabra = palabra.toUpperCase(); //Pasa todo el texto a mayúsculas
-			p = palabra.replaceAll("[ABCDEFGHIJKLMNÑOPQRSTUVWXYZ]", "_ "); //censura todas las letras por "_"
+			String palabra = s.nextLine();
+			palabra = palabra.toUpperCase();
+			
+		char[] palabraArray = new char[palabra.length()];
+		
+		for (int i = 0; i < palabra.length(); i++) {
+			palabraArray[i] = palabra.charAt(i);
+		}
+		
+		for (int i = 0; i < palabraArray.length; i++) {
+			System.out.print(palabraArray[i]);
+		}
 		
 		for (int i = vidas.length - 1; i >= 0 ; i--) { //Contador de vidas
 			System.out.println("");
@@ -48,19 +56,7 @@ public class Juego_del_colgado {
 					String letra = s.nextLine();
 					letra = letra.toUpperCase();
 					
-					for (int iii = 0; iii < palabra.length(); iii++) {
-						char cpalabra = palabra.charAt(iii);
-						char cletra = letra.charAt(0);
-						
-						if (cletra == cpalabra) {
-							
-							String P = "(?i)(?![" + cletra + "])\\w";
-							p = palabra.replaceAll(P, " _");
-							System.out.println("¡Acertaste!");
-							System.out.println("");
-						}
-						
-					}
+					
 				
 				
 			}
